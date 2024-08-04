fd54051e60cf386923c13467d49b6a4310e8c4a3
GavinDingCoolapk <16675113010@163.com>
2024-08-04 18:04:40 +0800

    added new logo, and adjusted the urls.py and settings.py for loading static whenDebug = False

--- dgxandwyx/urls.py
+++ dgxandwyx/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-
+from django.urls import path, re_path
+from django.views.static import serve
+from django.conf import settings
 from blog import views
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     path('', views.index),
     path('index.html', views.index),
     path('about', views.about),
