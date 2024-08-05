01f749c9000cc006f09dc4881c4c6beca5ded5cb
GavinDingCoolapk <16675113010@163.com>
2024-08-05 11:06:29 +0800

    DEBUG=True to debug admin login

--- dgxandwyx/settings.py
+++ dgxandwyx/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t+vn1wy0hc!5m#ou8a(mih)w*ep0m4uuurno2x))0g4q&cm3q%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
