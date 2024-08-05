3c08d01273aac4c72616d6c820ebd6f4ced8277a
GavinDingCoolapk <16675113010@163.com>
2024-08-05 10:48:04 +0800

    added admin and Post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Post(models.Model):
+    category = models.CharField(max_length=50)
+    title = models.CharField(max_length=150)
+    date = models.DateField(default=timezone.now)
+    author = models.CharField(max_length=10)
+    body = models.TextField()
+    image = models.ImageField(upload_to='blog/posts/%Y/%m/%d', blank=True)
--- dgxandwyx/urls.py
+++ dgxandwyx/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from blog import views
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     path('', views.index),
     path('index.html', views.index),
