935a5960626812a097554b5827f9388b050d94f2
GavinDingCoolapk <16675113010@163.com>
2024-08-06 10:51:06 +0800

    deleted post_style of posts and deleted several test posts

--- blog/models.py
+++ blog/models.py
@@ -12,19 +12,6 @@ class Post(models.Model):
 
     c_auth = [("Gavin", "Gavin"), ("Asinia", "Asinia")]
 
-    c_style = [(1, "1"),
-               (2, "2"),
-               (3, "3"),
-               (4, "4"),
-               (5, "5"),
-               (6, "6"),
-               (7, "7"),
-               (8, "8"),
-               (9, "9"),
-               (10, "10"),
-               (11, "11"),
-               ]
-
     category = models.CharField(choices=c_cat, max_length=50, default="Recent events")
     title = models.CharField(max_length=150)
     date = models.DateField(default=timezone.now)
@@ -34,4 +21,3 @@ class Post(models.Model):
     tag1 = models.CharField(max_length=15, blank=True)
     tag2 = models.CharField(max_length=15, blank=True)
     tag3 = models.CharField(max_length=15, blank=True)
-    post_style = models.IntegerField(choices=c_style, default=1)
